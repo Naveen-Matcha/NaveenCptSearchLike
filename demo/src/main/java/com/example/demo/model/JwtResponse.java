048c638b2ff70784dae977a1ec25847b64e3ae99
Naveen-Matcha <130667185+Naveen-Matcha@users.noreply.github.com>
2023-07-07 13:35:35 +0530

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/model/JwtResponse.java
@@ -0,0 +1,20 @@
+package com.example.demo.model;
+
+import java.io.Serializable;
+
+public class JwtResponse implements Serializable{
+	
+	private static final long serialVersionUID = -8091879091924046844L;
+	private final String jwt;
+
+    public JwtResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+	
+	
+
+}
